55e59962ad0f52b6404cd2294d61d16aa1161186
Nicola <nicola.sebastianelli@studio.unibo.it>
2017-04-14 16:56:21 +0200

    Add files via upload

--- Tron.py
+++ Tron.py
@@ -23,6 +23,7 @@ duserlist = list()
 cuserlist = list()
 playing = 0
 light=1
+REMOTE_SERVER = "www.google.com"
 
 def say(word,args):
         global playing
@@ -43,7 +44,7 @@ def play(m,arg):
                 bot.sendMessage(m.chat.id, "Avvio riproduzione canzoni")
                 say("Ok "+m.chat.first_name+", avvio riproduzione canzoni",1)
                 playing = 1
-                os.system("mpg321 -q -z -l 0 Music/*")
+                os.system("mpg321 -q -z -l 0 /home/pi/Desktop/Tron/Music/*")
 
 def stop(m):
         global playing
@@ -119,10 +120,10 @@ def rimuoviUtente(m,command):
 	global ruserlist
 	ok=-1
 	ruserlist.remove(m.chat.first_name)
-	f= open("Users.txt","r")
+	f= open("/home/pi/Desktop/Tron/Users.txt","r")
 	lines= f.readlines()
 	f.close()
-	f= open("Users.txt","w")
+	f= open("/home/pi/Desktop/Tron/Users.txt","w")
 	for line in lines:
 		if line!= command+"\n":
 			f.write(line)
@@ -140,10 +141,10 @@ def aggiungiUtente(m,command):
 	global auserlist
 	ok=1
 	auserlist.remove(m.chat.first_name)
-	f= open("Users.txt","r")
+	f= open("/home/pi/Desktop/Tron/Users.txt","r")
 	lines= f.readlines()
 	f.close()
-	f= open("Users.txt","w")
+	f= open("/home/pi/Desktop/Tron/Users.txt","w")
 	for line in lines:
 		if line == command+"\n":
 			f.write(line)
@@ -249,7 +250,15 @@ def downloadSong(url, fileLoc):
             break
         f.write(buff)
     f.close()
-	
+    
+def is_connected():
+  try:
+    host = socket.gethostbyname(REMOTE_SERVER)
+    s = socket.create_connection((host, 80), 2)
+    return True
+  except:
+     pass
+  return False	
 
 def handle(msg):
         global duserlist
@@ -264,12 +273,12 @@ def handle(msg):
 
 	print '[%s:%s - %s:%s] --> %s' % (m.chat.id, m.chat.username, str(datetime.datetime.now().hour),
 									  str(datetime.datetime.now().minute), command)
-	with open('Users.txt') as f:
+	with open('/home/pi/Desktop/Tron/Users.txt') as f:
 		array = [line.rstrip() for line in f]
 	f.close()
 	if m.chat.username in array:
 		ok = True
-	with open('Admin.txt') as f:
+	with open('/home/pi/Desktop/Tron/Admin.txt') as f:
 		array = [line.rstrip() for line in f]
 	f.close()
 	if m.chat.username in array:
@@ -363,7 +372,7 @@ def handle(msg):
                         thread.start_new_thread(say,(buff,1))
                         bot.sendMessage(chat_id, buff)
 		elif command == '/aggiungiutente':
-			with open('Admin.txt') as f1:
+			with open('/home/pi/Desktop/Tron/Admin.txt') as f1:
 				array1 = [line.rstrip() for line in f1]
 			f1.close()
 			if m.chat.username in array1:
@@ -374,7 +383,7 @@ def handle(msg):
 			thread.start_new_thread(say,(buff,1))
 			bot.sendMessage(chat_id, buff)
 		elif command == '/rimuoviutente':
-			with open('Admin.txt') as f1:
+			with open('/home/pi/Desktop/Tron/Admin.txt') as f1:
 				array1 = [line.rstrip() for line in f1]
 			f1.close()
 			if m.chat.username in array1:
@@ -394,11 +403,11 @@ def handle(msg):
 			thread.start_new_thread(say,("IP locale: "+buff,1))
 			s.close()
 		elif command == '/listautenti':
-			with open('Admin.txt') as f1:
+			with open('/home/pi/Desktop/Tron/Admin.txt') as f1:
 				array1 = [line.rstrip() for line in f1]
 			f1.close()
 			if m.chat.username in array1:
-				with open('Users.txt') as f2:
+				with open('/home/pi/Desktop/Tron/Users.txt') as f2:
 					array2 = [line.rstrip() for line in f2]
 				f2.close()
 				for user in array2:
@@ -409,7 +418,7 @@ def handle(msg):
 				bot.sendMessage(chat_id, buff)
 		elif command == '/sam':
 			i=random.randint(1, 6)
-			in_file = open("sam/"+str(i)+".jpeg", "r")
+			in_file = open("/home/pi/Desktop/Tron/sam/"+str(i)+".jpeg", "r")
 			bot.sendPhoto(chat_id, in_file)
 			in_file.close()
 		else:
@@ -426,6 +435,9 @@ def handle(msg):
 									 '-->Username')
 
 path = '/home/pi/Desktop/Tron/Music/'
+while not is_connected():
+        time.sleep(1)
+print 'Connesso a Internet ...'
 bot = telepot.Bot('324692150:AAENW7pqXh74gIn0ruxdbDeFUMytRqtlkaM')
 bot.message_loop(handle)
 print 'Tron Avviato ...'
